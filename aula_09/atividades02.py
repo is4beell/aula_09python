a01248f5108e3ae3e6f617413ab60fb3d38ce988
is4beell <is4beell@gmail.com>
2025-12-16 19:00:28 -0300

    Exercicio resolvido

--- aula_09/atividades02.py
+++ aula_09/atividades02.py
@@ -1,6 +1,6 @@
 #Programa para calcular IMC
 
-def calcule_imc(a, p):
+def calcule_imc(p, a):
     imc = p / (a * a)
     return imc
 
@@ -8,19 +8,19 @@ def calcule_imc(a, p):
 altura = float(input('Informe a altura:  '))
 peso = float(input('Informe o peso:  '))
 
-imc = calcule_imc(p / (a * a))
+imc = calcule_imc(peso, altura)
 
-if imc < 16.9:
+if imc <= 16.9:
     print('Muito abaixo do peso')
-elif imc < 18.4:
+elif imc <= 18.4:
     print('Abaixo do peso')
-elif imc < 24.9:
+elif imc <= 24.9:
     print('Peso normal')
-elif imc < 29.9:
+elif imc <= 29.9:
     print('Acima do peso')
-elif imc < 34.9:
+elif imc <= 34.9:
     print('Obesidade grau I')
-elif imc < 35:
+elif imc <= 35:
     print('Obesidade grau II')
 else:
     print('Obesidade grau III')
